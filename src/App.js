078e5aab2fee618721f3a88e407268b5c2bf793a
macbook <khaitruong1301@gmail.com>
2022-03-01 21:37:34 +0700

    react_b2_state_renderWithMap

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Databinding from './DataBinding/Databinding';
 import HandleEvent from './HandleEvent/HandleEvent';
+import Rendering_Conditions from './Rendering_Conditions/Rendering_Conditions';
+import Ex1StateDemo from './StateDemo/Ex1StateDemo';
+import RenderWithMap from './RenderWithMap/RenderWithMap';
+import QuanLyDanhSachPhim from './RenderWithMap/QuanLyDanhSachPhim';
 
 function App() {
   return (
     <div className="App">
       {/* <Databinding /> */}
-      <HandleEvent />
+      {/* <HandleEvent /> */}
+      {/* <Rendering_Conditions /> */}
+      {/* <Ex1StateDemo /> */}
+      {/* <RenderWithMap /> */}
+      <QuanLyDanhSachPhim />
     </div>
   );
 }
